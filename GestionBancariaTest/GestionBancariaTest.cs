2fcbea0d176b33abef5463799b85cc1ef4efae6b
BMS22-23 <bresen@alu.edu.gva.es>
2023-01-30 12:02:17 +0100

    Agrego primer caso de prueba para el método RealizarIngreso
    Uso de la nueva const SIN_ERR

--- GestionBancariaTest/GestionBancariaTest.cs
+++ GestionBancariaTest/GestionBancariaTest.cs
@@ -28,7 +28,7 @@ namespace GestionBancariaTest
             double saldoInicial = 30;
             double reintegro = 1;
             double saldoEsperado = saldoInicial - reintegro;
-            int salidaEsperada = 0;
+            int salidaEsperada = GestionBancariaApp.SIN_ERR;
 
             ProbarReintegro(saldoInicial, reintegro, saldoEsperado, salidaEsperada);
         }
@@ -38,7 +38,7 @@ namespace GestionBancariaTest
             double saldoInicial = 30;
             double reintegro = 2;
             double saldoEsperado = saldoInicial - reintegro;
-            int salidaEsperada = 0;
+            int salidaEsperada = GestionBancariaApp.SIN_ERR;
 
             ProbarReintegro(saldoInicial, reintegro, saldoEsperado, salidaEsperada);
         }
@@ -48,7 +48,7 @@ namespace GestionBancariaTest
             double saldoInicial = 30;
             double reintegro = 30;
             double saldoEsperado = saldoInicial - reintegro;
-            int salidaEsperada = 0;
+            int salidaEsperada = GestionBancariaApp.SIN_ERR;
 
             ProbarReintegro(saldoInicial, reintegro, saldoEsperado, salidaEsperada);
         }
@@ -58,7 +58,7 @@ namespace GestionBancariaTest
             double saldoInicial = 31;
             double reintegro = 30;
             double saldoEsperado = saldoInicial - reintegro;
-            int salidaEsperada = 0;
+            int salidaEsperada = GestionBancariaApp.SIN_ERR;
 
             ProbarReintegro(saldoInicial, reintegro, saldoEsperado, salidaEsperada);
         }
@@ -68,7 +68,7 @@ namespace GestionBancariaTest
             double saldoInicial = 60;
             double reintegro = 30;
             double saldoEsperado = saldoInicial - reintegro;
-            int salidaEsperada = 0;
+            int salidaEsperada = GestionBancariaApp.SIN_ERR;
 
             ProbarReintegro(saldoInicial, reintegro, saldoEsperado, salidaEsperada);
         }
@@ -131,6 +131,31 @@ namespace GestionBancariaTest
             Assert.AreEqual(salidaEsperada, salida, "Salida incorrecta.");
             Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001,
             "Se produjo un error al realizar el reintegro, saldo" +
+            "incorrecto.");
+        }
+
+
+
+        [TestMethod]
+        public void ValidarIngreso1()
+        {
+            double saldoInicial = 30;
+            double ingreso = 1;
+            double saldoEsperado = saldoInicial + ingreso;
+            int salidaEsperada = GestionBancariaApp.SIN_ERR;
+
+            ProbarIngreso(saldoInicial, ingreso, saldoEsperado, salidaEsperada);
+        }
+
+        private static void ProbarIngreso(double saldoInicial, double ingreso, double saldoEsperado, int salidaEsperada)
+        {
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+
+            // Método a probar
+            int salida = miApp.RealizarIngreso(ingreso);
+            Assert.AreEqual(salidaEsperada, salida, "Salida incorrecta.");
+            Assert.AreEqual(saldoEsperado, miApp.ObtenerSaldo(), 0.001,
+            "Se produjo un error al realizar el ingreso, saldo" +
             "incorrecto.");
         }
     }
